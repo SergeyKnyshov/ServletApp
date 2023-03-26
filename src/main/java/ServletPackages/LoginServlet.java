106dbcf1073da088ad8653d3e406eeb299ff411d
PC- <knyshov2002@mail.ru>
2023-03-26 20:18:06 +0500

    Initial commit

---
+++ src/main/java/ServletPackages/LoginServlet.java
@@ -0,0 +1,40 @@
+package ServletPackages;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+
+
+
+@WebServlet("/")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("login.jsp").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        HttpSession session = req.getSession();
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        if (req.getParameterValues("btnLog") != null ) {
+            String path = "/files" + "?path=C:\\Java\\" + username;
+            ServletContext servletContext = getServletContext();
+            RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher(path);
+            requestDispatcher.forward(req, resp);
+        }
+            req.getRequestDispatcher("login.jsp").forward(req, resp);
+
+    }
+}
